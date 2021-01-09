8b2e62a27d8cc609c36d4a109a2bce682f6dc2fe
Gabriel Jay Huerte <gjhuerte@gmail.com>
2021-01-09 22:34:15 +0800

    Remove pre-commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ChooseConnection from './components/ChooseConnection';
-import CreateMySQLConnection from './components/mysql/CreateMySQLConnection';
 import MySQLConnection from './components/mysql/MySQLConnection';
 
 export default function App() {
--- src/components/ChooseConnection.tsx
+++ src/components/ChooseConnection.tsx
@@ -5,21 +5,23 @@ import mongodbIcon from '../../app/images/mongodb.png';
 
 export default function ChooseConnection () {
   return (
-    <div class="p-10 flex w-screen h-screen">
-      <div class="flex flex-col justify-center content-stretch w-full">
+    <div className="p-10 flex w-screen h-screen">
+      <div className="flex flex-col justify-center content-stretch w-full">
         <div>
-          <div class="flex flex-row justify-center content-stretch mb-2">
-            <div class="p-5 rounded-lg bg-gray-50 shadow-md w-6/12">
-              <h3 class="text-gray-400 font-medium text-sm">CHOOSE CONNECTION</h3>
-              <ul class="flex flex-row my-2">
+          <div className="flex flex-row justify-center content-stretch mb-2">
+            <div className="p-5 rounded-lg bg-gray-50 shadow-md w-6/12">
+              <h3 className="text-gray-400 font-medium text-sm">
+                CHOOSE CONNECTION
+              </h3>
+              <ul className="flex flex-row my-2">
                 <li>
-                  <button class="text-gray-700 hover:text-gray-400 font-md font-medium mr-5 border-2 border-green-200 rounded-md focus:outline-none">
-                    <img src={mysqlIcon} class="h-20"/>
+                  <button className="text-gray-700 hover:text-gray-400 font-md font-medium mr-5 border-2 border-green-200 rounded-md focus:outline-none">
+                    <img src={mysqlIcon} className="h-20"/>
                   </button>
                 </li>
                 <li>
-                  <button class="text-gray-700 hover:text-gray-400 text-md font-medium mr-5 hover:border-green-200 hover:border-2 focus:outline-none">
-                    <img src={mongodbIcon} class="h-20"/>
+                  <button className="text-gray-700 hover:text-gray-400 text-md font-medium mr-5 hover:border-green-200 hover:border-2 focus:outline-none">
+                    <img src={mongodbIcon} className="h-20"/>
                   </button>
                 </li>
               </ul>
